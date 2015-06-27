4b5b2c837fac921043f1a29be24b7b61d72dfd05
Angristan <stanislas.lange@openmailbox.org>
2015-06-27 22:44:27 +0200

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -6,7 +6,7 @@ BLEU="\\033[1;34m"
 
 echo -e "${BLEU}""###########################################################################"
 echo -e "$BLEU""#                                                                         #"
-echo -e "$BLEU""#  ""$JAUNE""Ce script bash installe un serveur OpenDNS sur Debian 8 uniquement""$BLEU""     #"
+echo -e "$BLEU""#  ""$JAUNE""Ce script bash installe un serveur OpenVPN sur Debian 8 uniquement""$BLEU""     #"
 echo -e "$BLEU""# ""$JAUNE""Le serveur utilisera le protocole TCP sur le port 443 ainsi que OpenDNS""$BLEU"" #"
 echo -e "$BLEU""#                                                                         #"
 echo -e "$BLEU""###########################################################################"
